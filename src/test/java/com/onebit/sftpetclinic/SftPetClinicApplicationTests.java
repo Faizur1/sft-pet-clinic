8cf8694d0c1ede22cf3289405f99a39e253f35d4
faizur <faizur@gmail.com>
2023-03-25 11:41:07 +0600

    initial commit

---
+++ src/test/java/com/onebit/sftpetclinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebit.sftpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
